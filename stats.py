4b3947be9dcc4a61f747226b3ce33fff99560975
Waterhorse928 <72754883+Waterhorse928@users.noreply.github.com>
2021-07-02 09:01:57 -0700

    Stats
    Just added some stats using my Google spreadsheet

--- stats.py
+++ stats.py
@@ -12,10 +12,66 @@ def layton(level):
         
 def lea(level):
     if level == 1:
-        return {"vit":20,"atk":20,"mag":20,"skl":20,"spd":20,"luk":20,"dfn":20,"res":20}
+        return {'vit':21,'atk':21,'mag':21,'skl':21,'spd':21,'luk':21,'dfn':21,'res':21}
     if level == 2:
-        return {"vit":21,"atk":21,"mag":21,"skl":21,"spd":21,"luk":21,"dfn":21,"res":21}
-
+        return {'vit':22,'atk':22,'mag':22,'skl':22,'spd':22,'luk':22,'dfn':22,'res':22}
+    if level == 3:
+        return {'vit':23,'atk':23,'mag':23,'skl':23,'spd':23,'luk':23,'dfn':23,'res':23}
+    if level == 4:
+        return {'vit':24,'atk':24,'mag':24,'skl':24,'spd':24,'luk':24,'dfn':24,'res':24}
+    if level == 5:
+        return {'vit':25,'atk':25,'mag':25,'skl':25,'spd':25,'luk':25,'dfn':25,'res':25}
+    if level == 6:
+        return {'vit':26,'atk':26,'mag':26,'skl':26,'spd':26,'luk':26,'dfn':26,'res':26}
+    if level == 7:
+        return {'vit':27,'atk':27,'mag':27,'skl':27,'spd':27,'luk':27,'dfn':27,'res':27}
+    if level == 8:
+        return {'vit':28,'atk':28,'mag':28,'skl':28,'spd':28,'luk':28,'dfn':28,'res':28}
+    if level == 9:
+        return {'vit':29,'atk':29,'mag':29,'skl':29,'spd':29,'luk':29,'dfn':29,'res':29}
+    if level == 10:
+        return {'vit':30,'atk':30,'mag':30,'skl':30,'spd':30,'luk':30,'dfn':30,'res':30}
+    if level == 11:
+        return {'vit':31,'atk':31,'mag':31,'skl':31,'spd':31,'luk':31,'dfn':31,'res':31}
+    if level == 12:
+        return {'vit':32,'atk':32,'mag':32,'skl':32,'spd':32,'luk':32,'dfn':32,'res':32}
+    if level == 13:
+        return {'vit':33,'atk':33,'mag':33,'skl':33,'spd':33,'luk':33,'dfn':33,'res':33}
+    if level == 14:
+        return {'vit':34,'atk':34,'mag':34,'skl':34,'spd':34,'luk':34,'dfn':34,'res':34}
+    if level == 15:
+        return {'vit':35,'atk':35,'mag':35,'skl':35,'spd':35,'luk':35,'dfn':35,'res':35}
+    if level == 16:
+        return {'vit':36,'atk':36,'mag':36,'skl':36,'spd':36,'luk':36,'dfn':36,'res':36}
+    if level == 17:
+        return {'vit':37,'atk':37,'mag':37,'skl':37,'spd':37,'luk':37,'dfn':37,'res':37}
+    if level == 18:
+        return {'vit':38,'atk':38,'mag':38,'skl':38,'spd':38,'luk':38,'dfn':38,'res':38}
+    if level == 19:
+        return {'vit':39,'atk':39,'mag':39,'skl':39,'spd':39,'luk':39,'dfn':39,'res':39}
+    if level == 20:
+        return {'vit':40,'atk':40,'mag':40,'skl':40,'spd':40,'luk':40,'dfn':40,'res':40}
+    if level == 21:
+        return {'vit':41,'atk':41,'mag':41,'skl':41,'spd':41,'luk':41,'dfn':41,'res':41}
+    if level == 22:
+        return {'vit':42,'atk':42,'mag':42,'skl':42,'spd':42,'luk':42,'dfn':42,'res':42}
+    if level == 23:
+        return {'vit':43,'atk':43,'mag':43,'skl':43,'spd':43,'luk':43,'dfn':43,'res':43}
+    if level == 24:
+        return {'vit':44,'atk':44,'mag':44,'skl':44,'spd':44,'luk':44,'dfn':44,'res':44}
+    if level == 25:
+        return {'vit':45,'atk':45,'mag':45,'skl':45,'spd':45,'luk':45,'dfn':45,'res':45}
+    if level == 26:
+        return {'vit':46,'atk':46,'mag':46,'skl':46,'spd':46,'luk':46,'dfn':46,'res':46}
+    if level == 27:
+        return {'vit':47,'atk':47,'mag':47,'skl':47,'spd':47,'luk':47,'dfn':47,'res':47}
+    if level == 28:
+        return {'vit':48,'atk':48,'mag':48,'skl':48,'spd':48,'luk':48,'dfn':48,'res':48}
+    if level == 29:
+        return {'vit':49,'atk':49,'mag':49,'skl':49,'spd':49,'luk':49,'dfn':49,'res':49}
+    if level == 30:
+        return {'vit':50,'atk':50,'mag':50,'skl':50,'spd':50,'luk':50,'dfn':50,'res':50}
+    
 def marisa(level):
     if level == 1:
         return {"vit":18,"atk":17,"mag":30,"skl":20,"spd":25,"luk":13,"dfn":15,"res":22}
@@ -26,7 +82,11 @@ def nitori(level):
         
 def orin(level):
     if level == 1:
-        return {"vit":16,"atk":17,"mag":22,"skl":19,"spd":32,"luk":29,"dfn":10,"res":15}
+        return {'vit':16,'atk':18,'mag':23,'skl':19,'spd':34,'luk':29,'dfn':12,'res':17}
+    if level == 2:
+        return {'vit':17,'atk':19,'mag':24,'skl':20,'spd':37,'luk':30,'dfn':12,'res':17}
+    if level == 3:
+        return {'vit':17,'atk':20,'mag':24,'skl':20,'spd':40,'luk':32,'dfn':13,'res':18}
         
 def sanae(level):
     if level == 1:
@@ -46,5 +106,9 @@ def suwako(level):
 
 def utsuho(level):
     if level == 1:
-        return {"vit":27,"atk":29,"mag":32,"skl":12,"spd":2,"luk":4,"dfn":28,"res":26}
+        return {'vit':28,'atk':30,'mag':35,'skl':13,'spd':2,'luk':5,'dfn':29,'res':26}
+    if level == 2:
+        return {'vit':29,'atk':32,'mag':37,'skl':14,'spd':2,'luk':5,'dfn':30,'res':27}
+    if level == 3:
+        return {'vit':30,'atk':34,'mag':39,'skl':14,'spd':2,'luk':6,'dfn':31,'res':28}
 
